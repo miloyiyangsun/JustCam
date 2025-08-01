31dcd4f13e6b7434458265e5cb06bb7d8dc01af8
milo <114107432+miloyiyangsun@users.noreply.github.com>
2025-08-01 16:33:05 +0200

    Initial Commit

---
+++ JustCam/JustCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustCamApp.swift
+//  JustCam
+//
+//  Created by 孙懿旸 on 01/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct JustCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
